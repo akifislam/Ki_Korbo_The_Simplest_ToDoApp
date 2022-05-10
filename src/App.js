ec0da9d4d3436d47d632da0824a80f3312f01116
Akif Islam <iamakifislam@gmail.com>
2022-05-10 17:00:43 +0600

    implement add feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todolists, settodolist] = useState([
+    "Morning Walk at 6am",
+    "Exercise at 8am",
+    "Eat breakfast at 9am",
+  ]);
+  const [currentInput, setCurrentInput] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>
+        Ki Korbo? - A Simple To Do App
+        <small class="text-muted">
+          <h6>Made with React by Akif Islam </h6>
+        </small>
+      </h1>
+      <br></br>
+
+      <ul class="list-group list-group-flush">
+        {todolists.map((todo) => (
+          <li class="list-group-item">{todo}</li>
+        ))}
+      </ul>
+
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter a task to do"
+          aria-label="Recipient's username"
+          aria-describedby="button-addon2"
+          onChange={(e) => setCurrentInput(e.target.value)}
+        ></input>
+        <button
+          class="btn btn-outline-secondary"
+          type="button"
+          id="button-addon2"
+          onClick={(e) => {
+            e.preventDefault();
+            settodolist([...todolists, currentInput]);
+            setCurrentInput("");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+      </div>
     </div>
   );
 }
